88af8cd35da929ee45fd7103c58c01b9d4e72e40
Sakria88 <sakriaobido@gmail.com>
2026-02-24 23:46:21 +0000

    fixed floor

--- Game Engines/Assets/Scripts/InfiniteFloor.cs
+++ Game Engines/Assets/Scripts/InfiniteFloor.cs
@@ -3,31 +3,45 @@ using UnityEngine;
 
 public class InfiniteFloor : MonoBehaviour
 {
-    public Transform playerTransform;
-    public static InfiniteFloor Instance;
-    
     // Based on your Scale Z
     private float floorLength = 214.695f;
-
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        // Check if the player has moved beyond the current floor piece
-        float halfLength = floorLength / 2f;
-        // If the player has moved past the end of this floor piece
-        if (playerTransform.position.z > transform.position.z + floorLength)
+        if (other.CompareTag("Player"))
         {
-            Vector3 newPos = transform.position;
-            newPos.z += floorLength * 2; // Move it ahead by two lengths (since we have 2 pieces total)
-            transform.position = newPos;
-        }
-        // If the player has moved before the start of this floor piece
-        if (playerTransform.position.z < transform.position.z - floorLength)
-        {
-            Vector3 newPos = transform.position;
-            newPos.z -= floorLength * 2;
-            transform.position = newPos;
+            // Move the floor piece ahead of the player
+            transform.parent.position += new Vector3(0, 0, floorLength * 2);
         }
     }
+
+}
+    
+    
+    // void Update()
+    // {
+    //     // Check if the player has moved beyond the current floor piece
+    //     float halfLength = floorLength / 4f;
+    //     // If the player has moved past the end of this floor piece
+    //     if (playerTransform.position.z > transform.position.z + floorLength)
+    //     {
+    //         Vector3 newPos = transform.position;
+    //         newPos.z += floorLength * 2; // Move it ahead by two lengths (since we have 2 pieces total)
+    //         transform.position = newPos;
+    //     }
+    //     // If the player has moved before the start of this floor piece
+    //     if (playerTransform.position.z < transform.position.z - floorLength)
+    //     {
+    //         Vector3 newPos = transform.position;
+    //         newPos.z -= floorLength * 2;
+    //         transform.position = newPos;
+    //     }
+    // }
+
+
+
+
+
+
     // void Update()
     // {
     //     float buffer = 0.5f; // A small buffer to prevent visual gaps when the player moves fast
@@ -46,4 +60,3 @@ public class InfiniteFloor : MonoBehaviour
     //         transform.position = newPos;
     //     }
     // }
-}

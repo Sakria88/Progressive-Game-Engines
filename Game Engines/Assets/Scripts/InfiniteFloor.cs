5fdc9521f7d0601466d82fcdc3f1dc60cd3feb0f
Sakria88 <sakriaobido@gmail.com>
2026-02-24 23:10:24 +0000

    fixing floor

--- Game Engines/Assets/Scripts/InfiniteFloor.cs
+++ Game Engines/Assets/Scripts/InfiniteFloor.cs
@@ -1,20 +1,49 @@
+using System;
 using UnityEngine;
 
 public class InfiniteFloor : MonoBehaviour
 {
     public Transform playerTransform;
+    public static InfiniteFloor Instance;
+    
     // Based on your Scale Z
-    private float floorLength = 214.695f; 
+    private float floorLength = 214.695f;
 
     void Update()
     {
+        // Check if the player has moved beyond the current floor piece
+        float halfLength = floorLength / 2f;
         // If the player has moved past the end of this floor piece
-        if (playerTransform.position.z > transform.position.z + (floorLength / 2))
+        if (playerTransform.position.z > transform.position.z + floorLength)
         {
-            // Move it ahead by two lengths (since we have 2 pieces total)
             Vector3 newPos = transform.position;
-            newPos.z += floorLength * 2;
+            newPos.z += floorLength * 2; // Move it ahead by two lengths (since we have 2 pieces total)
+            transform.position = newPos;
+        }
+        // If the player has moved before the start of this floor piece
+        if (playerTransform.position.z < transform.position.z - floorLength)
+        {
+            Vector3 newPos = transform.position;
+            newPos.z -= floorLength * 2;
             transform.position = newPos;
         }
     }
+    // void Update()
+    // {
+    //     float buffer = 0.5f; // A small buffer to prevent visual gaps when the player moves fast
+    //     // If the player has moved past the end of this floor piece
+    //     if (playerTransform.position.z > transform.position.z + (floorLength / 2) + buffer)
+    //     {
+    //         // Move it ahead by two lengths (since we have 2 pieces total)
+    //         Vector3 newPos = transform.position;
+    //         newPos.z += floorLength * 2;
+    //         transform.position = newPos;
+    //     }
+    //     else if (playerTransform.position.z < transform.position.z - (floorLength / 2) - buffer)
+    //     {
+    //         Vector3 newPos = transform.position;
+    //         newPos.z -= floorLength * 2;
+    //         transform.position = newPos;
+    //     }
+    // }
 }
